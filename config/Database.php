b1f76518398699e4e863a3c43be43f2e8c36316c
Hamza Sehouli <sehouli.hamza@gmail.com>
2021-11-29 23:03:07 +0100

    Finished adduser view

--- config/Database.php
+++ config/Database.php
@@ -7,10 +7,8 @@ class Database
     public static function connect()
     {
         try {
-
             $con = new \PDO("mysql:host=localhost;port=3306;dbname=php_crud", 'root', '');
             $con->setAttribute(\PDO::ERRMODE_EXCEPTION, \PDO::ATTR_ERRMODE);
-
             return $con;
         } catch (\PDOException$e) {
             print_r($e->getMessage());
--- views/_addUser.php
+++ views/_addUser.php
@@ -1,30 +1,57 @@
 <?php
 
+use app\config\Database;
+
+$con = Database::connect();
+
 if(!empty($_POST)){
-    // print_r($_POST);
+
+    extract($_POST);
+
+    if(empty($name)){
+        echo 'Please enter a name';
+        exit;
+    }
+    if(empty($email)){
+        echo 'Please enter a valid email';
+        exit;
+    }
+
+    $query =!empty($_FILES['image']['tmp_name']) ? "INSERT INTO user (name, email,image) VALUES(:name, :email,:image)" : "INSERT INTO user (name, email) VALUES(:name, :email)";
+  
+    $stmt = $con->prepare($query);
+    $stmt->bindValue(':name', $name);
+    $stmt->bindValue(':email', $email);
+    if(!empty($_FILES['image']['tmp_name'])){
+         $generateImageName = function($num){
+            $chars = '1234567890QWERTZUIOPLKJHGFDSAYXCVBNMqwertzuioplkjhgfdsayxcvbnm';
+            $imageName = $chars[3];
+            $charsLen = strlen($chars);
+            // echo $randoNum;
+            for($i = 0; $i <= $num; $i++){
+                $randoNum = rand(1,$charsLen);
+                $imageName .= $chars[$randoNum];
+            }
+            return $imageName;
+        };
+    
+        $imageName = $generateImageName(20);
+    
+        
+    
+        move_uploaded_file($_FILES["image"]["tmp_name"],$_SERVER['DOCUMENT_ROOT'] . '/public/images/'.$imageName.'.png');
+        $stmt->bindValue(':image', $imageName);
+    }
+    $stmt->execute();
+
+   
+
+   
+    
+       
 
 }
-if(!empty($_FILES)){
-    
-    $generateImageName = function($num){
-        $chars = '1234567890QWERTZUIOPLKJHGFDSAYXCVBNMqwertzuioplkjhgfdsayxcvbnm';
-        $imageName = '';
-        $charsLen = strlen($chars);
-        // echo $randoNum;
-        for($i = 0; $i <= $num; $i++){
-            $randoNum = rand(0,$charsLen);
-            $imageName .= $chars[$randoNum];
-        }
-        return $imageName;
-    };
 
-    $imageName = $generateImageName(20);
-
-    
-
-    move_uploaded_file($_FILES["image"]["tmp_name"],$_SERVER['DOCUMENT_ROOT'] . '/public/images/'.$imageName.'.png');
-
-}
 
 ?>
 
