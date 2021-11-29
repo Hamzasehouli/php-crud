f2f1db75fa32105fdabff5d88e24fe7200c65386
Hamza Sehouli <sehouli.hamza@gmail.com>
2021-11-29 19:53:42 +0100

    Finished viewControllers class

---
+++ controllers/ViewControllers.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\controllers;
+
+class ViewControllers{
+    public static function overview()
+    {
+        echo 'overview';
+    }
+     public static function addUser()
+    {
+        echo 'add user';
+    }
+     public static function updateUser()
+    {
+        echo 'updtaes users';
+    }
+    
+}
--- index.php
+++ index.php
@@ -4,11 +4,13 @@ require_once './vendor/autoload.php';
 
 use app\config\Database;
 use app\controllers\UserControllers;
+use app\controllers\ViewControllers;
 use app\Router;
 
 Database::connect();
 
 $router = new Router();
+
 
 if (str_starts_with($_SERVER['REQUEST_URI'], '/api/v1/users')) {
     $router->get('/api/v1/users', [UserControllers::class, 'getAllUsers']);
@@ -16,4 +18,12 @@ if (str_starts_with($_SERVER['REQUEST_URI'], '/api/v1/users')) {
     $router->get('/api/v1/users/getuser', [UserControllers::class, 'getUser']);
     $router->update('/api/v1/users/updateuser', [UserControllers::class, 'updateUser']);
     $router->delete('/api/v1/users/deleteuser', [UserControllers::class, 'deleteUser']);
+    $router->run();
 }
+
+
+
+$router->get('/', [ViewControllers::class, 'overview']);
+$router->get('/adduser', [ViewControllers::class, 'addUser']);
+$router->get('/updateuser', [ViewControllers::class, 'updateUser']);
+$router->run();
